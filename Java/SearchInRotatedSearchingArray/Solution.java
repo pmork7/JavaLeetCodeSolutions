63e61299d94d26eb08574e5f9f95a3e38eddab37
Paul Mork <paul.a.mork@gmail.com>
2022-11-07 14:48:21 -0800

    Added files

--- Java/SearchInRotatedSearchingArray/Solution.java
+++ Java/SearchInRotatedSearchingArray/Solution.java
@@ -2,24 +2,19 @@ import java.util.*;
 import java.math.*;
 
 public class Solution {
-    public static int findZero(int[] nums) {
+    /* Find offset by pulling the last element from the array. */
+    public static int findOffset(int[] nums) {
         return nums[nums.length - 1];
     }
-
+    
+    /* Helper method to call findOffset() and binary search methods and return result */
     public static int search(int[] nums, int target) {
-        int offset = findZero(nums);
-        System.out.println(offset);
+        int offset = findOffset(nums);
         int result = modifiedBinarySearch(nums, target, offset);
         return result;
     }
     
-    /*public static int modulo(int n, int m) {
-      if (n % m > 0) {
-         return n % m;
-      }
-      return 
-    }*/
-    
+    /* A modified binary search that uses the rotation offset to search the correct indices */
     public static int modifiedBinarySearch(int[] nums, int target, int offset) {
       int min = 0;
       int max = nums.length - 1;      
@@ -38,8 +33,7 @@ public class Solution {
     }
     
     public static void main(String[] args) {
-      int[] nums = {4,5,6,7,8,9,10,0,1,2,3};
-      System.out.println(Arrays.toString(nums));
-      System.out.println(search(nums, 10));
+      int[] nums = {4,5,6,7,0,1,2};
+      System.out.println(search(nums, 0));
     }
 }
