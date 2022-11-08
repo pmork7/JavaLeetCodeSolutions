02bf0e5082e3256f6d8d75b049df4de34b16c621
Paul Mork <paul.a.mork@gmail.com>
2022-11-08 12:45:55 -0800

    Created directory for permutations problem

---
+++ Java/Permutations/Permutations.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Permutations {
+   public static List<List<Integer>> permute(int[] nums) {
+      return null;
+   }
+   
+   public static void main(String[] args) {
+      
+   }
+}
